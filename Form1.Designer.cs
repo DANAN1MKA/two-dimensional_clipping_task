66e4dfec1b5bf2bae674cc4309cf779e8a1f97df
DANAN1MKA <drujochek22@gmail.com>
2019-11-01 16:49:54 +0300

    Added interface

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Laba_5_CG
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pB_Canv = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pB_Canv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pB_Canv
+            // 
+            this.pB_Canv.BackColor = System.Drawing.Color.White;
+            this.pB_Canv.Location = new System.Drawing.Point(0, 0);
+            this.pB_Canv.Name = "pB_Canv";
+            this.pB_Canv.Size = new System.Drawing.Size(801, 450);
+            this.pB_Canv.TabIndex = 0;
+            this.pB_Canv.TabStop = false;
+            this.pB_Canv.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pB_Canv.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pB_Canv_MouseMove);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pB_Canv);
+            this.Name = "Window";
+            this.Text = "Laba_5";
+            ((System.ComponentModel.ISupportInitialize)(this.pB_Canv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pB_Canv;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_5_CG
+{
+    //Вариант 1. Общий алгоритм отсечения отрезка ортогональным окном
+
+
+    public partial class Window : Form
+    {
+        private Point[] poligon = new Point[8];
+
+        //Изображение преднозначеное для стирания изменений при отрисовке временных линий событием pB_Canv_MouseMove
+        private Bitmap cls;
+
+
+        public Window()
+        {
+            InitializeComponent();
+
+            //задаем полигон
+            poligon[0] = new Point(185, 199);
+            poligon[1] = new Point(182, 96);
+            poligon[2] = new Point(268, 91);
+            poligon[3] = new Point(272, 144);
+            poligon[4] = new Point(370, 143);
+            poligon[5] = new Point(376, 69);
+            poligon[6] = new Point(489, 120);
+            poligon[7] = new Point(478, 234);
+
+            //рисуем полигон
+            Bitmap canv = new Bitmap(pB_Canv.Width, pB_Canv.Height);
+            Graphics g = Graphics.FromImage(canv);
+            Pen p = new Pen(Color.White, 2);
+            g.Clear(Color.Black);
+            for(int i = 1; i < 8; i++)
+            {
+                g.DrawLine(p, poligon[i - 1].X, poligon[i - 1].Y, poligon[i].X, poligon[i].Y);
+            }
+            g.DrawLine(p, poligon[0].X, poligon[0].Y, poligon[7].X, poligon[7].Y);
+            pB_Canv.BackgroundImage = canv;
+
+            //запоминаем исходное изображение
+            cls = canv;
+        }
+
+        //Флаг сигналезирующий о необходимости рисовать линию
+        private bool second_point_flag = false;
+        //первая точка линии
+        private Point point;
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Bitmap canv = new Bitmap(pB_Canv.BackgroundImage);
+            Graphics g = Graphics.FromImage(canv);
+
+            if(second_point_flag) { second_point_flag = false; }
+            else
+            {
+                //запоминаем точку и начинаем рисовать линию
+                point = new Point(e.X, e.Y);
+                second_point_flag = true;
+            }
+            pB_Canv.BackgroundImage = canv;
+        }
+
+        private void pB_Canv_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (second_point_flag)
+            {
+                //Стираем предыдущее изображение
+                pB_Canv.BackgroundImage = cls;
+
+                //рисуем новую линию
+                Bitmap canv = new Bitmap(pB_Canv.BackgroundImage);
+                Graphics g = Graphics.FromImage(canv);
+                g.DrawLine(new Pen(Color.White), point.X, point.Y, e.X, e.Y);
+                pB_Canv.BackgroundImage = canv;
+            }
+        }
+    }
+}
